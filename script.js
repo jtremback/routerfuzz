751f776a6685740b4cf38275a5ab98e115d01531
Jehan Tremback <jehan.tremback@gmail.com>
2013-03-24 02:09:07 -0700

    more better scripts

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 var targets = [
-	{single: "192.168.0.1"},
-	{single: "pony"},
-	{single: "172.30.3.242"}
+	{single: "pony", tryreq: "pony.jpeg"},
+	{single: "192.168.0.1", tryreq: "pony.jpeg"},
+	{single: "172.30.3.242", tryreq: "pony.jpeg"}
 ]
 
-try {
-	scanner(targets);
+scanner(targets);
+
+// try {
+// 	scanner(targets);
 // }
 
 // catch(e) {
 // 	console.log(e);
 // }
 
+// document.body.innerHTML = "<img src='http://pony/pony.jpg'>";
+
+
 function scanner(targets) {
 	for (var i = targets.length - 1; i >= 0; i--) {
-		console.log(targets[i].single)
-		document.getElementById("holder").innerHTML = "<img src='";
+		document.body.innerHTML = "<img id='img' onload='correct();' src='http://" + targets[i].single + "/pony.jpeg' />"
 	};
 }
+
+
+function correct() {
+	console.log("damn!")
+}
+
 
 function httpGet(theUrl) {
 	var req = null;
 
 	req = new XMLHttpRequest();
-	req.open( "GET", "http://" + theUrl );
+	req.open( "POST", "http://" + theUrl );
 	req.send( null );
 	return req.response;
 	req.onerror = function() { console.log("error"); }
